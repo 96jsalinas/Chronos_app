b271d8a71deafe0772a56275497a014c1d9c3bca
Josu <96jsalinas@gmail.com>
2018-04-18 11:51:10 +0300

    Added customize screen

--- app/src/main/java/com/example/a96jsa/chronos_app/Customize.java
+++ app/src/main/java/com/example/a96jsa/chronos_app/Customize.java
@@ -1,29 +1,32 @@
 package com.example.a96jsa.chronos_app;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainScreen extends AppCompatActivity {
+public class Customize extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_customize);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        RadioButton categoryButton = findViewById(R.id.catRadioButton);
+        radioGroup.check(categoryButton.getId());
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -54,17 +57,6 @@ public class MainScreen extends AppCompatActivity {
                         return true;
                     }
                 });
-
-        FloatingActionButton launchCustomize = (FloatingActionButton) findViewById(R.id.fab);
-        launchCustomize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent dynIntent = new Intent(getBaseContext(),Customize.class);
-                startActivity(dynIntent);
-            }
-        });
-
-
     }
 
     @Override
@@ -83,4 +75,5 @@ public class MainScreen extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
